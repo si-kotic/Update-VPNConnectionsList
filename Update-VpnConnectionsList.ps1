b4ca4cf72ea26f6e79f44c3b46a741a8c460073c
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-02-27 11:31:26 +0000

    First Commit

---
+++ Update-VpnConnectionsList.ps1
@@ -0,0 +1,16 @@
+$curUserName = $env:USERNAME
+$vpnConfigSrc = "\\brick2\files\Data\ServiceDesk\VPN"
+$vpnConfigDest = "C:\Users\$curUserName\AppData\Roaming\Microsoft\Network\Connections\Pbk"
+$shortDate = (Get-Date).ToString("yyyyMMdd")
+
+if (!(Get-Item -Path "$vpnConfigSrc\rasphone.pbk")) {
+    Add-VpnConnection -Name "InitialVPN" -ServerAddress "vpn.test.com"
+    $remFlag = $true
+}
+Set-Location $vpnConfigSrc
+Rename-Item -Path "$vpnConfigDest\rasphone.pbk" -NewName "rasphone.pbk_$shortDate.bak"
+Copy-Item -Path .\rasphone.pbk -Destination $vpnConfigDest -Force
+if ($remFlag) {
+    Remove-VpnConnection -Name "InitialVPN"
+}
+Set-Location $HOME
